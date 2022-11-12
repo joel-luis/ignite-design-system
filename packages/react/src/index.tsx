dd1c565e558970bf868dda6fead1ed1b480583e4
Joel Luis <joelluis73@gmail.com>
2022-11-12 13:23:02 -0300

    create design system

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,5 @@
 import { colors } from '@ignite-ui/tokens'
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <h1 style={{ color: colors.ignite300 }}>Hello Worldd</h1>
 }
