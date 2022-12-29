8d1f44ee55350e36ca7a5a9cae917d471c3fb582
Joel Luis <joelluis73@gmail.com>
2022-12-29 09:52:37 -0300

    create component Box

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -118,30 +118,13 @@ var {
   }
 });
 
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite300",
+// src/components/Box.tsx
+var Box = styled("div", {
+  padding: "$4",
   borderRadius: "$md",
-  fontWeight: "bold",
-  color: "$white",
-  border: 0,
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4"
-      },
-      large: {
-        fontSize: 16,
-        padding: "$3 $4"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
+  backgroundColor: "$gray800",
+  border: "1px solid $gray600"
 });
 export {
-  Button
+  Box
 };
