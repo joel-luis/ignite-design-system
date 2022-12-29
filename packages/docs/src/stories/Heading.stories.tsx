08436fab7891502460b4cb0b818ca497bd0dc353
Joel Luis <joelluis73@gmail.com>
2022-12-29 11:25:00 -0300

    create component Heading and Avatar

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Heading',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+export const customTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podemos alterar com a propriedade `as`.',
+      },
+    },
+  },
+}
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -153,7 +153,78 @@ var Text = styled("p", {
     size: "md"
   }
 });
+
+// src/components/Heading.tsx
+var Heading = styled("h2", {
+  fontFamily: "$default",
+  lineHeight: "$shorter",
+  margin: 0,
+  color: "$gray100",
+  variants: {
+    size: {
+      sm: { fontSize: "$xl" },
+      md: { fontSize: "$2xl" },
+      lg: { fontSize: "$4xl" },
+      "2xl": { fontSize: "$5xl" },
+      "3xl": { fontSize: "$6xl" },
+      "4xl": { fontSize: "$7xl" },
+      "5xl": { fontSize: "$8xl" },
+      "6xl": { fontSize: "$9xl" }
+    }
+  },
+  defaultVariants: {
+    size: "md"
+  }
+});
+
+// src/components/Avatar/index.tsx
+import { User } from "phosphor-react";
+
+// src/components/Avatar/styles.ts
+import * as Avatar from "@radix-ui/react-avatar";
+var AvatarContainer = styled(Avatar.Root, {
+  borderRadius: "$full",
+  display: "inline-block",
+  width: "$12",
+  height: "$12",
+  overflow: "hidden"
+});
+var AvatarImage = styled(Avatar.Image, {
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "inherit"
+});
+var AvatarFallback = styled(Avatar.Fallback, {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "$gray600",
+  color: "$gray800",
+  svg: {
+    width: "$6",
+    height: "$6"
+  }
+});
+
+// src/components/Avatar/index.tsx
+import { jsx, jsxs } from "react/jsx-runtime";
+function Avatar2(props) {
+  return /* @__PURE__ */ jsxs(AvatarContainer, {
+    children: [
+      /* @__PURE__ */ jsx(AvatarImage, __spreadValues({}, props)),
+      /* @__PURE__ */ jsx(AvatarFallback, {
+        delayMs: 600,
+        children: /* @__PURE__ */ jsx(User, {})
+      })
+    ]
+  });
+}
 export {
+  Avatar2 as Avatar,
   Box,
+  Heading,
   Text
 };
